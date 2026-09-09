3f10929f715357e2a1696717dbf332fe964afc6c
gigi-nyam <nyamamoto@gig-intelligence.jp>
2026-09-09 17:22:00 +0900

    Add custom food entry form

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='calorie-pwa-v3';
+const CACHE='calorie-pwa-v4';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
